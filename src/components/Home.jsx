388ffef41215346a7dd2038bd67bb6b4c913ddce
laurybeth <laurybeth@msn.com>
2023-02-08 00:07:13 -0500

    maquetado y estilo a hu1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,22 +16,45 @@ export function Home () {
   }
 
   return (
-    <section>
-      <div>
-        <h1 >SIGN IN</h1>
-        <form >
+    <div className="card">
+      <div
+      className="card-header">
+        <img className="logo" src='../assets/logo-shadow.png'></img>
+      </div>
+      <div className="card-body">
+        <section className='container__login'>
+        <form className="d-grid gap-2 col-9 mx-auto" >{/* onSubmit={}  */}
+        <label
+        htmlFor="formEmailInput"
+        className="form-label text-white">Email
+        </label>
           <input
           type="text"
-          placeholder=" User"
+          className="form-control form-input"
+          placeholder="name@example.com"
             name='email'
           />
+          <label htmlFor="formPasswordInput"
+          className="form-label text-white">Password
+          </label>
           <input
           type="password"
-          placeholder="Password"
+          className="form-control form-input"
+          placeholder="********"
           name='password'
           />
-          <button> Login</button>
+          <div
+          className="d-grid gap-3 col-12 mx-auto">
+          <button
+          type="submit"
+          className="btn btn-primary btn-lg btn-login ">Login
+          </button>
+          </div>
         </form>
+        </section>
       </div>
-    </section>)
+      <div className="card-footer text-muted">
+         <div className='footer-text'>Copyright © 2023</div>
+      </div>
+      </div>)
 }
