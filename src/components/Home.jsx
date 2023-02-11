f4fee9a8ddc372729948bd561fe0f5c43796b920
laurybeth <laurybeth@msn.com>
2023-02-11 11:03:27 -0500

    identación de maquetado de home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,37 +17,20 @@ export function Home () {
 
   return (
     <div className="card">
-      <div
-      className="card-header">
+      <div className="card-header">
         <img className="logo" src='../assets/logo-shadow.png'></img>
       </div>
       <div className="card-body">
         <section className='container__login'>
         <form className="d-grid gap-2 col-9 mx-auto" >{/* onSubmit={}  */}
-        <label
-        htmlFor="formEmailInput"
-        className="form-label text-white">Email
+        <label htmlFor="formEmailInput" className="form-label text-white">Email
         </label>
-          <input
-          type="text"
-          className="form-control form-input"
-          placeholder="name@example.com"
-            name='email'
-          />
-          <label htmlFor="formPasswordInput"
-          className="form-label text-white">Password
+          <input type="text" className="form-control form-input" placeholder="name@example.com" name='email'/>
+          <label htmlFor="formPasswordInput" className="form-label text-white">Password
           </label>
-          <input
-          type="password"
-          className="form-control form-input"
-          placeholder="********"
-          name='password'
-          />
-          <div
-          className="d-grid gap-3 col-12 mx-auto">
-          <button
-          type="submit"
-          className="btn btn-primary btn-lg btn-login ">Login
+          <input type="password" className="form-control form-input" placeholder="********" name='password'/>
+          <div className="d-grid gap-3 col-12 mx-auto">
+          <button type="submit" className="btn btn-primary btn-lg btn-login ">Login
           </button>
           </div>
         </form>
