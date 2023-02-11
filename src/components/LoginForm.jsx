be9d9e7f320e553dd842214f6f7beccc7f7ac760
laurybeth <laurybeth@msn.com>
2023-02-11 13:25:52 -0500

    se creó funcionalidad para el ingreso de email con mensaje de error descriptivo

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { validateEmail } from '../controllers/login-controller'
 
 const initialForm = {
   id: null,
@@ -8,6 +9,7 @@ const initialForm = {
 
 export function LoginForm () {
   const [form, setForm] = useState(initialForm)
+  const [warning, setWarning] = useState('')
 
   function handleSubmit (e) {
     setForm(
@@ -19,18 +21,27 @@ export function LoginForm () {
   }
 
   function handleChange (e) {
-
+    const email = e.target.value
+    if (!validateEmail(email)) {
+      setWarning('The format does not match what was requested. Example: name@example.com')
+    }
   }
 
   return (
             <section className='container__login'>
-            <form className="d-grid gap-2 col-9 mx-auto" onSubmit={handleSubmit} >{/*  */}
-            <label htmlFor="formEmailInput" className="form-label text-white">Email
-            </label>
-              <input type="text" className="form-control form-input" name='email' placeholder="name@example.com" onChange={handleChange} value={form.email}/>
-              <label htmlFor="formPasswordInput" className="form-label text-white">Password
+            <form className="d-grid gap-2 col-9 mx-auto" onSubmit={handleSubmit} >
+              <div className='container-input'>
+              <label htmlFor="email" className="form-label text-white">Email
               </label>
-              <input type="password" className="form-control form-input" name='password' placeholder="********" onChange={handleChange} value={form.password}/>
+              <input type="text" className="form-control form-input" name='email' placeholder="name@example.com" onChange={handleChange}/>
+              <p className='errorMessage'>{warning}</p>
+              </div>
+              <div className='container-input'>
+              <label htmlFor="password" className="form-label text-white">Password
+              </label>
+              <input type="password" className="form-control form-input " name='password' placeholder="********" />
+              <p>{}</p>
+              </div>
               <div className="d-grid gap-3 col-12 mx-auto">
               <button type="submit" className="btn btn-primary btn-lg btn-login ">Login
               </button>
---
+++ src/controllers/login-controller.jsx
@@ -0,0 +1,4 @@
+export function validateEmail (email) {
+  const regexEmail = /[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{1,5}/
+  return regexEmail.test(email)
+}
