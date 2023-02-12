eac07b251cbd5c7bc0472454fcd8976661a13ca6
laurybeth <laurybeth@msn.com>
2023-02-12 18:27:00 -0500

    instalación de testing library

--- test/login.spec.js
+++ test/login.spec.js
@@ -1,4 +1,9 @@
-// import { Login } from "../src/components/Login"
+// import { render, screen, fireEvent } from '@testing-library/react'
+import { Login } from '../src/components/Login'
 
 // eslint-disable-next-line jest/no-focused-tests
-describe.only('Login')
+describe.only('Login', () => {
+  it('Should be', () => {
+    Login()
+  })
+})
