d35c8e47d7240a7a26922ed4556e2ec0302a3ae1
laurybeth <laurybeth@msn.com>
2023-02-14 18:15:55 -0500

    configuración de deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/DEV001-burger-queen-api-client/',
   server: {
     host: '127.0.0.1'
   }
