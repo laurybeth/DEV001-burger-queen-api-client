a32e39aa02b497db2dde54ca9624d14949e858ae
laurybeth <laurybeth@msn.com>
2023-02-17 19:34:31 -0500

    creación de LoggedPanel

---
+++ src/components/LoggedPanel.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export function LoggedPanel () {
+  return (
+    <div className='card'>
+
+    </div>
+
+  )
+}
--- src/components/WaiterPanel.jsx
+++ src/components/WaiterPanel.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Footer } from './Footer'
+import { LoggedPanel } from './LoggedPanel'
 
 export function WaiterPanel () {
   return (
   <div className='card'>
 
-  <Footer/>
+  <Footer element = { <LoggedPanel/> }/>
   </div>
 
   )
