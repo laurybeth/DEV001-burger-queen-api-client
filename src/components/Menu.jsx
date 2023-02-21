cf40016f0892c695b466a1842c0a337ea369a720
laurybeth <laurybeth@msn.com>
2023-02-21 16:50:54 -0500

    estilo a componente product

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,25 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { httpRequest } from '../fetch-api/httpRequest'
 import { Product } from './Product'
 
 export function Menu () {
-  function showMenu (e) {
-
+  const [products, setProducts] = useState([])
+  async function handleMenu (e) {
+    const productType = e.target.name
+    try {
+      const response = await httpRequest().get(`http://localhost:8080/products?type=${productType}`)
+      setProducts(response)
+      console.log(products)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
     <div className='card container-menu'>
       <div className="card-header">
-        <ul className="nav container-nav">
+        <ul className="nav container-nav justify-content-center">
         <li className="nav-item">
-            <a className="nav-link active nav-text" href="#">Breakfast</a>
+            <a className="nav-link active nav-text" href="#" onClick={handleMenu} name='Breakfast'>Breakfast</a>
         </li>
         <li className="nav-item">
-            <a className="nav-link nav-text" href="#">Lunch & Dinner</a>
+            <a className="nav-link nav-text" href="#" onClick={handleMenu} name='Lunch & Dinner'>Lunch & Dinner</a>
         </li>
         </ul>
       </div>
-      <div className="card-body">
-        <Product/>
+      <div className="card-body">{products.map((product) => {
+        return (
+                <Product
+                  key={product.id}
+                  id={product.id}
+                  name={product.name}
+                  description={product.description}
+                  image={product.image}
+                  price={product.price}
+
+                />
+        )
+      })}
+
       </div>
     </div>
   )
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
-export function Product () {
+export function Product ({ id, name, description, price, image }) {
+  console.log('product', id)
   return (
-    <div className='card'>
-       <div className="card-body">
+    <div className='card container-product'>
+     <div className="card-body body-product">
+      <div className='left-side'>
+        <div className='text-product'>{name}</div>
+        <div className='text-product text-product-description'>{description}</div>
+      </div>
+      <div className='right-side'><img src={`${image}`}/></div>
      </div>
+     <div className='card-footer body-product'>
+      <div className='text-product left-side'>{price}</div>
+      <div className='text-product right-side'><img></img>{1}<img></img></div>
      </div>
+    </div>
   )
 }
